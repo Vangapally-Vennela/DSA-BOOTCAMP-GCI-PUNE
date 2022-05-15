11d41146d3ba85522f67e9310306cbe51724e121
Vangapally-Vennela <94056568+Vangapally-Vennela@users.noreply.github.com>
2022-05-15 11:37:00 +0530

    Create ReverseArray.java

---
+++ V.Vennela/Basics of Programming/Recursion and Backtracking/Recursion In Arrays/ReverseArray.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class ReverseArray{
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        displayArrReverse(a,n-1);
+    }
+
+    public static void displayArrReverse(int[] arr, int idx) 
+    {
+        if(idx<0)
+            return ;
+        
+        System.out.println(arr[idx]);
+        displayArrReverse(arr,idx-1);
+    }
+
+}

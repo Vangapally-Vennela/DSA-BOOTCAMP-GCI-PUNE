54e449f75afe1de7baee31862bddbcdd626f5102
Vangapally-Vennela <94056568+Vangapally-Vennela@users.noreply.github.com>
2022-05-15 11:34:36 +0530

    Create DisplayArray.java

---
+++ V.Vennela/Basics of Programming/Recursion and Backtracking/Recursion In Arrays/DisplayArray.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class DisplayArray{
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        displayArr(a,n-1);
+    }
+
+    public static void displayArr(int[] arr, int idx)
+    {
+        if(idx<0)
+            return ;
+        displayArr(arr,idx-1);
+        System.out.println(arr[idx]);
+    }
+
+}
